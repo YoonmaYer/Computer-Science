edd0b2304c7335f25b94ea792fac294b9624cbd4
YoonmaYer <rkddbs0813@gmail.com>
2022-04-05 18:11:00 +0900

    20220405

--- Data_Structure/Linkedlist/Homework.c
+++ Data_Structure/Linkedlist/Homework.c
@@ -6,21 +6,20 @@ struct NODE {
 	struct NODE *link
 };
 
-int main(void)
+
+int main()
 {
-	int sel;
-	
-	while (1)
-	{
+
+
 		printf("1. 노드 추가\n");
 		printf("2. 노드 삭제\n");
 		printf("3. 노드 검색\n");
 		printf("4. 노드 출력\n");
 		printf("5. 종료\n");
 		pritnf("=================\n");
-		scanf("%d, &sel") // 사용자 선택 
-		
-	}
-	return 0;
- } 
+		scanf("%d, &sel"); // 사용자 선택 
+}
+	
+
  
+
